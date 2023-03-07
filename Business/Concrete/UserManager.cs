2cdf99bde2c65f3badc231c8756954e334a44745
Özge Bektaş <ozgebek.dev@gmail.com>
2023-03-07 17:16:27 +0300

    Controlers added,Web Apı added, postman is checked

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -1,4 +1,8 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +11,23 @@ using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
-    public class UserManager:IUserService
+    public class UserManager : IUserService
     {
+        IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public IDataResult<List<User>> GetAll()
+        {
+            if (DateTime.Now.Hour == 12)
+            {
+                return new ErrorDataResult<List<User>>(Messages.MaintenanceTime);
+            }
+            return new SuccessDataResult<List<User>>(_userDal.GetAll());
+
+        }
     }
 }
