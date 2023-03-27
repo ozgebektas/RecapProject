09affc1a1dfd7188e8e404df4678ded71b5a38d7
Özge Bektaş <ozgebek.dev@gmail.com>
2023-03-27 15:59:10 +0300

    caching,transaction and performance added

--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,8 +1,11 @@
-﻿using Core.Utilities.Ioc;
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.Utilities.Ioc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
