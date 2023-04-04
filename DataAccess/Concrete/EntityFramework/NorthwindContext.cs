630d5179b6052306fe478efe5c846b6d447452ae
Ozge Bektas <ozgebektas53@gmail.com>
2023-04-04 18:34:01 +0300

    startup.cs updating

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -13,7 +13,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server = (localdb)\MSSQLLocalDb;Database = ReCapDb; Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server = (localdb)\MSSQLLocalDb;Database =DbCar; Trusted_Connection=true");
         }
         public DbSet<Car> Cars { get; set; }
         public DbSet<Color> Colors { get; set; }
